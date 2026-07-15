02996069a7e8aa1abd04ea1b1dfd041650ca4e21
maf1ck <kosmanmatvij@gmail.com>
2026-07-15 19:12:50 +0300

    refactor: comment out historyApiFallback in vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    historyApiFallback: true,
+    // historyApiFallback: true,
   },
   build: {
     cssMinify: true,
